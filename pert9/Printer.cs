0a17bec4d5ed1ede8f44f05acf9bc4f3ea9b5c9f
Haitsama <haitsam@students.amikom.ac.id>
2023-06-12 00:01:16 +0700

    Add project files.

---
+++ pert9/Printer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymerphism
+{
+    public class Printer
+    {
+        public string NamaPrinter { get; set; }
+        public virtual void show()
+        {
+            Console.WriteLine("\ndisplay dimensian");
+        }
+        public virtual void print()
+        {
+            Console.WriteLine("\ndisplay dimensian");
+        }
+    }
+    public class Epson : Printer
+    {
+        public override void show()
+        { 
+            Console.WriteLine("Epson display dimension : 10*11");
+        }
+        public override void print()
+        {
+            Console.WriteLine("Epson printer printing....");
+        }
+    }
+    public class Canon : Printer
+    {
+        public override void show()
+        {
+            Console.WriteLine("Canon display dimension : 9.5*12");
+        }
+        public override void print()
+        {
+            Console.WriteLine("Canon printer printing....");
+        }
+    }
+    public class LasetJet : Printer
+    {
+        public override void show()
+        {
+            Console.WriteLine("LasetJet display dimension : 11*12");
+        }
+        public override void print()
+        {
+            Console.WriteLine("LasetJet printer printing....");
+        }
+    }
+}
---
+++ pert9/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Polymerphism;
+
+namespace pert9
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Printer printer;
+            Console.WriteLine("Pilih printer : ");
+            Console.WriteLine("1. Epson");
+            Console.WriteLine("2. Canon");
+            Console.WriteLine("3. LaserJet");
+
+            Console.WriteLine("Nomor printer [1...3] :");
+
+            int nomorprinter = Convert.ToInt32(Console.ReadLine());
+
+            if (nomorprinter == 1)
+                printer = new Epson();
+            else if (nomorprinter == 2)
+                printer = new Canon();
+            else if (nomorprinter == 3)
+                printer = new LasetJet();
+            else
+            {
+                Console.WriteLine("Nomor printer tidak tersedia");
+                return;
+            }
+
+            printer.show();
+            printer.print();
+
+            Console.ReadKey();
+        }
+    }
+}
